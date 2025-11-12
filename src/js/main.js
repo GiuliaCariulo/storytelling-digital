430eac0e889c9f794b5190eb9147f81287cbdf24
GiuliaCariulo <98399966+GiuliaCariulo@users.noreply.github.com>
2025-11-12 14:33:29 +0100

    hero

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,17 @@
 import { gsap } from "gsap";
+gsap.registerPlugin(ScrollTrigger);
+
+// HERO PARALLAX_______________________________________________________
+
+gsap.to(".hero-mountain-1", {
+  y: 0,
+  x: -300,
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".hero",
+    start: "top bottom",
+    end: "bottom top",
+    scrub: true,
+    markers: true,
+  },
+});
