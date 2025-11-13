7f11c2371779c87e6402f91c83bfc2992a2baab3
GiuliaCariulo <98399966+GiuliaCariulo@users.noreply.github.com>
2025-11-13 10:15:38 +0100

    Sabre's walking on the top of comic box

--- src/js/main.js
+++ src/js/main.js
@@ -17,3 +17,25 @@ let hero = gsap.timeline({
 hero.fromTo(".hero-mountain-2", { x: 0, y: 0 }, { x: 800, y: 47 }, 0);
 hero.fromTo(".hero-mountain-3", { x: 0, y: 0 }, { x: -800, y: 47 }, 0);
 hero.fromTo(".hero-title", { scale: 1, y: 0 }, { scale: 0.7, y: -100 }, 0);
+hero.fromTo(".hero-title", { y: -100 }, { scale: 0.7, y: -500 });
+
+// Sabre's walking on comic box_______________________________________________________
+
+const comicHorizontalScroll = document.querySelector(
+  ".comic-scroll-horizontal"
+);
+const scrollWidth = comicHorizontalScroll.scrollWidth - window.innerWidth;
+
+gsap.to(comicHorizontalScroll, {
+  x: -scrollWidth,
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".comic-scroll-container",
+    start: "top 20%",
+    end: () => `+=${scrollWidth}`,
+    scrub: 1,
+    pin: true,
+    anticipatePin: 1,
+    markers: true,
+  },
+});
