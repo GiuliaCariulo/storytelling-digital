1b442144c5648ae247fd4cc9bae49cb85fc97380
GiuliaCariulo <98399966+GiuliaCariulo@users.noreply.github.com>
2025-11-20 17:05:30 +0100

    tests double sabre

--- src/js/main.js
+++ src/js/main.js
@@ -4,21 +4,21 @@ gsap.registerPlugin(ScrollTrigger);
 
 // HERO PARALLAX_______________________________________________________
 
-// let hero = gsap.timeline({
-//   scrollTrigger: {
-//     trigger: ".hero",
-//     start: "top top",
-//     // end: "+=100%",
-//     scrub: 1,
-//     pin: true,
-//   },
-// });
+let hero = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".hero",
+    start: "top top",
+    // end: "+=100%",
+    scrub: 1,
+    pin: true,
+  },
+});
 
-// hero.fromTo(".hero-mountain-1", { y: 0 }, { y: -500 }, 0);
-// hero.fromTo(".hero-mountain-2", { x: 0, y: 0 }, { x: 800, y: 47 }, 0);
-// hero.fromTo(".hero-mountain-3", { x: 0, y: 0 }, { x: -800, y: 47 }, 0);
-// hero.fromTo(".hero-title", { scale: 1, y: 0 }, { scale: 0.7, y: -100 }, 0);
-// hero.fromTo(".hero-title", { y: -100 }, { scale: 0.7, y: -500 });
+hero.fromTo(".hero-mountain-1", { scale: 1.5 }, { scale: 1 }, 0);
+hero.fromTo(".hero-mountain-2", { x: 0 }, { x: 800 }, 0);
+hero.fromTo(".hero-mountain-3", { x: 0 }, { x: -800 }, 0);
+hero.fromTo(".hero-title", { scale: 1, y: 0 }, { scale: 0.4, y: -100 }, 0);
+hero.fromTo(".hero-title", { y: -100 }, { y: -400 });
 
 // Sabre's walking on comic box_______________________________________________________
 
