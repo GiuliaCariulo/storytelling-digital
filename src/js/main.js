bbb061077994cf58da2ff006aa2139d75740ffa3
GiuliaCariulo <98399966+GiuliaCariulo@users.noreply.github.com>
2025-11-22 21:18:51 +0100

    sabre apparition

--- src/js/main.js
+++ src/js/main.js
@@ -8,15 +8,28 @@ let hero = gsap.timeline({
   scrollTrigger: {
     trigger: ".hero",
     start: "top top",
-    // end: "+=100%",
     scrub: 1,
     pin: true,
   },
 });
 
+sabre = document.querySelector(".sabre");
+heroSabre = document.querySelector(".hero-sabre");
+
+function sabreExchange() {
+  console.log("sabre exchange");
+  sabre.style.opacity = 1;
+  heroSabre.style.opacity = 0;
+}
+
 hero.fromTo(".hero-mountain-1", { scale: 1.5 }, { scale: 1 }, 0);
 hero.fromTo(".hero-mountain-2", { x: 0 }, { x: 800 }, 0);
-hero.fromTo(".hero-mountain-3", { x: 0 }, { x: -800 }, 0);
+hero.fromTo(
+  ".hero-mountain-3",
+  { x: 0 },
+  { x: -800, onComplete: sabreExchange },
+  0
+);
 hero.fromTo(".hero-title", { scale: 1, y: 0 }, { scale: 0.4, y: -100 }, 0);
 hero.fromTo(".hero-title", { y: -100 }, { y: -400 });
 
